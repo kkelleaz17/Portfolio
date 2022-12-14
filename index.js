2c715a09051e0ec30228cf42052d13002317544a
kkelleaz17 <111470082+kkelleaz17@users.noreply.github.com>
2022-12-14 16:11:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("HAM").addEventListener("click",()=>{
+    if(document.getElementById("NAV2").style.display == "none"){
+    document.getElementById("NAV2").style.display="flex"
+    }else{
+        document.getElementById("NAV2").style.display = "none"
+    }
+})
